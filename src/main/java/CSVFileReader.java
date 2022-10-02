82707a5f6201d7eb60fe83327abe789e4b2284dd
peters <peters1111@gmail.com>
2022-10-02 01:05:20 -0700

    create data_access_object

--- src/main/java/CSVFileReader.java
+++ src/main/java/CSVFileReader.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class CSVFileReader {
     public static void main(String[] args) {
 
+    }
+
+    public static void DAO(){
+        String filePath = "C:\\Users\\ABRAHAM\\Desktop\\People-data.csv";
+        try{
+            BufferedReader lineReader = new BufferedReader(new FileReader(filePath));
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
